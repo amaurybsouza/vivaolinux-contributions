19cc9bda04a5ffa0d4e66d43c800d7b40b757dc3
amaurybsouza <amaurybsouza@gmail.com>
2020-05-29 22:53:39 -0300

    new script for user accounts

---
+++ Shell scripts/create-user.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "Starting new script for user create"
+echo ""
+echo "Select an option below to start
+
+1- Create complete user 
+2- Create simple user
+3- Delete user accounts
+4- Change user configurations
+5- Create new group
+6- Delete groups
+7- Change groups configurations
+0- Exit user tool "
+echo ""
+echo -n "Select an option"
+read option
+case $option in
+	1)
+		echo "Amaury"
+
